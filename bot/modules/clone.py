5b74b4b809eacdaf0c7b236cab4a2a624af91181
BaashaXclouD <96932663+BaashaXclouD@users.noreply.github.com>
2022-02-11 20:46:55 +0530

    2.0

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -152,7 +152,7 @@ def cloneNode(update, context):
         else:
             sendLog(result + cc + msg_g, context.bot, update, button)
             auto = sendMessage(result + cc + fwdpm, context.bot, update)
-            Thread(target=auto_delete, args=(bot, update.message, auto)).start()
+            Thread(target=auto_delete, args=(context.bot, update.message, auto)).start()
             sendPrivate(result + cc + msg_g, context.bot, update, button)
         if is_gdtot:
             LOGGER.info(f"Deleting: {link}")
