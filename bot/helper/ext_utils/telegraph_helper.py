8d6fd7f05e1c6479303dc099398a33a53f780110
Kristy-offl <81421572+kristy-offl@users.noreply.github.com>
2022-02-13 15:39:46 +0530

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -57,4 +57,4 @@ class TelegraphHelper:
 			return self.edit_page(path, title, content)
 
 
-telegraph=TelegraphHelper('BaashaX', 'https://t.me/BaashaXclouD')
+telegraph=TelegraphHelper('KaipullaBots', 'https://t.me/KaipullaBots')
